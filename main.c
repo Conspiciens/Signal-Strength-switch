3f6eac201490c6d4966728c84fcad3d01da69788
Conspiciens <fernandezjohann08@gmail.com>
2024-10-02 13:06:16 -0700

    Added wifi and cellular name

--- main.c
+++ main.c
@@ -1,7 +1,19 @@
 #include <stdio.h> 
+#include <stdlib.h> 
+#include <string.h> 
+#include <sys/socket.h>
+#include <sys/un.h> 
+#include <unistd.h> 
 
+#define CONTROL_INTERFACE = "/var/run/wpa_supplicant"
 
+int get_wifi_strength(const char *iface) {
+    
+
+} 
 
 int main() {
+    const char *wifi_iface = "wlan0"; 
+
 
 } 
