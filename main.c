8086191bd74615f08b1658f16035ca989ccf4919
Conspiciens <fernandezjohann08@gmail.com>
2024-10-02 13:32:54 -0700

    init for wifi strength

--- main.c
+++ main.c
@@ -2,11 +2,27 @@
 #include <stdlib.h> 
 #include <string.h> 
 #include <sys/socket.h>
-#include <sys/un.h> 
 #include <unistd.h> 
+
+#include <netlink/netlink.h> 
+#include <netlink/genl/genl.h> 
+#include <netlink/genl/ctrl.h> 
+#include <linux/nl082211.h> 
+#include <errno.h> 
 
 
 int get_wifi_strength(const char *iface) {
+    struct nl_sock *sock; 
+    struct nl_msg *msg; 
+
+    sock = nl_socket_alloc(); 
+    if (!sock) {
+        printf("Failed to allocate sock for nl"); 
+        return -1; 
+    } 
+
+    
+        
     
 
 } 
