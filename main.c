129b146748d270281b5fbecb0a5b116d667a058e
Conspiciens <fernandezjohann08@gmail.com>
2024-10-11 11:16:26 -0700

    Initial commit for files

--- main.c
+++ main.c
@@ -21,8 +21,38 @@ int get_wifi_strength(const char *iface) {
         return -1; 
     } 
 
+    /* Connect to the generic link */ 
+    if (genl_connect(sock)) {
+        printf("Failed to connect to generic link"); 
+        nl_socket_free(sock); 
+        return -1; 
+    }      
+
+    /* Connect socket to driver */ 
+    driver_id = genl_ctrl_recieve(sock, "nl80211"); 
+    if (driver_id < 0) {
+        printf("Driver not found"); 
+        nl_socket_free(sock); 
+        return -1; 
+    } 
+
+    /* Look for the interface index */ 
+    if_index = if_nametoindex(iface); 
+    if (if_index == 0) {
+        printf("Interface not found"); 
+        nl_socket_free(sock); 
+        return -1; 
+    } 
+
+    /* Allocate the msg */ 
+    msg = nlmsg_alloc(); 
+    if (!msg) {
+        printf("Failed to allocate netlink message"); 
+        nl_socket_free(sock); 
+        return -1; 
+    } 
+
     
-        
     
 
 } 
